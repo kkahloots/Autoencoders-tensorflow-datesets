975b82c9206ed9223152c6cf5706d4900518b39f
Khalid Kahloot <kkahloots@gmail.com>
2019-10-31 19:02:49 +0100

    Add files via upload

--- bases/base_graph.py
+++ bases/base_graph.py
@@ -11,17 +11,17 @@ from networks.deconv_net import DeconvNet3
 
 class BaseGraph:
     def __init__(self, configuration):
-        with tf.variable_scope('global_step'):
-            self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
-        
-        with tf.variable_scope('cur_epoch'):
-            self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
-            self.increment_cur_epoch_tensor = tf.assign(self.cur_epoch_tensor, self.cur_epoch_tensor + 1)
+        self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
+
+        self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
 
         self.config = configuration
         self.x_flat_dim = self.config.width * self.config.height * self.config.num_channels
         self.extra_sittings()
         self.build_graph()
+
+    def increment_cur_epoch_tensor(self):
+        self.cur_epoch_tensor.assign(self.cur_epoch_tensor + 1)
 
     def extra_sittings(self):
         pass
@@ -108,61 +108,19 @@ class BaseGraph:
                                      ENCODER-DECODER
     ------------------------------------------------------------------------------ 
     '''
-
-    def create_encoder(self, input_, hidden_dim, output_dim, num_layers, transfer_fct, \
-                       act_out, reuse, kinit, bias_init, drop_rate, prefix, isConv):
-        latent_ = DenseNet(input_=input_,
-                           hidden_dim=hidden_dim,
-                           output_dim=output_dim,
-                           num_layers=num_layers,
-                           transfer_fct=transfer_fct,
-                           act_out=act_out,
-                           reuse=reuse,
-                           kinit=kinit,
-                           bias_init=bias_init,
-                           drop_rate=drop_rate,
-                           prefix=prefix) if not isConv else \
-            ConvNet3(input_=input_,
-                     hidden_dim=hidden_dim,
-                     output_dim=output_dim,
-                     num_layers=num_layers,
-                     transfer_fct=transfer_fct,
-                     act_out=act_out,
-                     reuse=reuse,
-                     kinit=kinit,
-                     bias_init=bias_init,
-                     drop_rate=drop_rate,
-                     prefix=prefix)
-        return latent_
-
-    def create_decoder(self, input_, hidden_dim, output_dim, num_layers, transfer_fct, \
-                       act_out, reuse, kinit, bias_init, drop_rate, prefix, isConv):
-        recons_ = DenseNet(input_=input_,
-                           hidden_dim=hidden_dim,
-                           output_dim=output_dim,
-                           num_layers=num_layers,
-                           transfer_fct=transfer_fct,
-                           act_out=act_out,
-                           reuse=reuse,
-                           kinit=kinit,
-                           bias_init=bias_init,
-                           drop_rate=drop_rate,
-                           prefix=prefix) if not isConv else \
-            DeconvNet3(input_=input_,
-                       num_layers=num_layers,
-                       hidden_dim=hidden_dim,
-                       output_dim=output_dim,
-                       width=self.config.width,
-                       height=self.config.height,
-                       nchannels=self.config.num_channels,
-                       transfer_fct=transfer_fct,
-                       act_out=act_out,
-                       reuse=reuse,
-                       kinit=kinit,
-                       bias_init=bias_init,
-                       drop_rate=drop_rate,
-                       prefix=prefix)
-        return recons_
-
-
+    def create_net(self, input_, hidden_dim, output_dim, num_layers, transfer_fct, act_out, \
+                   reuse, kinit, bias_init, drop_rate, prefix, isConv):
+        net_function = ConvNet3 if isConv else DenseNet
+        net_ = net_function(input_=input_,
+                               hidden_dim=hidden_dim,
+                               output_dim=output_dim,
+                               num_layers=num_layers,
+                               transfer_fct=transfer_fct,
+                               act_out=act_out,
+                               reuse=reuse,
+                               kinit=kinit,
+                               bias_init=bias_init,
+                               drop_rate=drop_rate,
+                               prefix=prefix)
+        return net_
 
